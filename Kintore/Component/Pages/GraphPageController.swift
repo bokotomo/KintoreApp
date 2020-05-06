c78dc7a999b298cf6a54f9952d7b56f49f9580b5
bokotomo <bokotomo@me.com>
2020-05-06 10:45:57 +0900

    feat

--- Kintore/Component/Pages/GraphPageController.swift
+++ Kintore/Component/Pages/GraphPageController.swift
@@ -1,9 +1,13 @@
 import UIKit
 
 class GraphPageController: UIViewController {
+    // アクション系は階層分けた方が良さそう
+    @IBAction func AddGraphButton(_ sender: UIButton) {
+        print("設定ページを開く")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 }
-
---
+++ Kintore/Component/Pages/VideoPageController.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+class VideoPageController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
